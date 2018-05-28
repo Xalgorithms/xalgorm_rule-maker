f77e96c75b945b9e1933ecbd3283d146c189743e
Hayk <hayk.pilos@gmail.com>
2018-05-28 19:53:08 +0400

    docs(license): Add license
    Change-Id: I71d0fa1279bdb4756a9db80a4adc29e89e8b83a7

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+// Copyright 2018 Hayk Pilosyan <hayk.pilos@gmail.com>
+// This file is part of Interlibr, a functional component of an
+// Internet of Rules (IoR).
+// ACKNOWLEDGEMENTS
+// Funds: Xalgorithms Foundation
+// Collaborators: Don Kelly, Joseph Potvin and Bill Olders.
+// Licensed under the Apache License, Version 2.0 (the "License"); you
+// may not use this file except in compliance with the License. You may
+// obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
